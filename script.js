e75297d41b116f1c8b52703ee73c0bc8c8a38edd
Azmain Khan Jesun <azmainjesun13@gmail.com>
2026-07-06 20:53:58 +0600

    Implemented add expense feature

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Expense Tracker Started");
+function addExpense() {
+
+    let name = document.getElementById("expenseName").value;
+    let amount = document.getElementById("expenseAmount").value;
+
+    let list = document.getElementById("expenseList");
+
+    let item = document.createElement("li");
+
+    item.textContent = name + " - " + amount + " BDT";
+
+    list.appendChild(item);
+
+}
